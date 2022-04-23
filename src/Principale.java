f17c852f5a40ca023ad1710d33592fb1af30079f
4BI <4BI@DESKTOP-4I0NS9T>
2022-04-23 12:07:22 +0200

    version 1.10

--- src/Principale.java
+++ src/Principale.java
@@ -2,11 +2,11 @@
  * 
  * @author michele
  *
- * @version 1.00
+ * @version 1.10
  * 
  */
 public class Principale {
 	public static void main (String args[]) {
-		System.out.println("prova gitHub 1");
+		System.out.println("prova gitHub 3");
 	}
 }
